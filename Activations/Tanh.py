52a5ef0b1fe83125adadb111e795fabc1efb8b2d
KirthanKamble <kirthan.kamble@gmail.com>
2024-06-03 20:55:38 +0530

    ADD: Tanh activation

--- Activations/Tanh.py
+++ Activations/Tanh.py
@@ -1,2 +1,8 @@
 from activation import Activation
 from numpy import tanh
+
+class Tanh(Activation):
+    def __init__(self):
+        tanh_fn = lambda z : tanh(z)
+        tanh_prime = lambda z : 1 - tanh(z)**2
+        super().__init__(tanh_fn, tanh_prime)
