41ffee7930193b4e38da9c9e31dfbc95d28c3abb
KirthanKamble <kirthan.kamble@gmail.com>
2024-06-03 20:57:33 +0530

    ADD: Sigmoid activation

--- Activations/sigmoid.py
+++ Activations/sigmoid.py
@@ -1,5 +1,8 @@
 from activation import Activation
-import numpy as np
+from numpy import exp
 
 class Sigmoid(Activation):
-    def __init__(self, )
+    def __init__(self):
+        sigmoid = lambda z : 1/(1+exp(-z))
+        sigmoid_prime = lambda z : (sigmoid(z)) * (1 - sigmoid(z))
+        super().__init__(sigmoid, sigmoid_prime)
