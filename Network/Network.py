3d1b9903236320d1c695c2753f7f9e2ae37443d2
KirthanKamble <kirthan.kamble@gmail.com>
2024-06-03 22:42:44 +0530

    ADD: Network class

--- Network/Network.py
+++ Network/Network.py
@@ -14,4 +14,18 @@ class Network:
     
     def train(self, X, y, epochs, lr):
         for epoch in range(epochs):
-            
+            error = 0
+            for X_train, y_train in zip(X, y):
+                # forward pass
+                y_pred = self.predict(X_train)
+
+                # Error
+                error += self.loss_fn.loss(y_train, y_pred)
+
+                # Backward
+                grad = self.loss_fn.gradient(y_train, y_pred)
+                for layer in reversed(self.achitecture):
+                    grad = layer.backward(grad, lr)
+
+                error /= len(X_train)
+                print(f"Completed {epoch+1} epoch, loss = {error}")
