3f70a2cdf3adcf76b6f098f8fecbaa2f1c6cb7ff
KirthanKamble <kirthan.kamble@gmail.com>
2024-07-13 18:35:58 +0530

    ADD: Reshape layer

---
+++ Layers/Reshape.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Reshpae:
+    def __init__(self, input_shape, output_shape):
+        self._input_shpae = input_shape
+        self._output_shpae = output_shape
+
+    def forward(self, input_data):
+        return np.reshape(input_data, self._output_shpae)
+    
+    def backward(self, output_grad, learning_rate=None):
+        return np.reshape(output_grad, self._input_shpae)
